e6020ffce93eefeeed0af230e81e0f298937140d
kosm-dev <72253360+kosm-dev@users.noreply.github.com>
2023-03-23 16:15:39 +0200

    added lang btn

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,33 @@
-$(function () {
+// Получаем все дропдауны
+
+const dropdowns = document.querySelectorAll('.dropdown');
+
+dropdowns.forEach(dropdown => {
+  const select = dropdown.querySelector('.select'),
+    caret = dropdown.querySelector('.caret'),
+    menu = dropdown.querySelector('.menu'),
+    options = dropdown.querySelectorAll('.menu li'),
+    selected = dropdown.querySelector('.selected');
+
+  select.addEventListener('click', () => {
+    select.classList.toggle('select-clicked');
+    caret.classList.toggle('caret-rotate');
+    menu.classList.toggle('menu-open');
+  });
+
+  options.forEach(option => {
+    option.addEventListener('click', () => {
+      selected.innerText = option.innerText;
+
+      select.classList.remove('select-clicked');
+      caret.classList.remove('caret-rotate');
+      menu.classList.remove('menu-open');
+
+      options.forEach(option => {
+        option.classList.remove('active');
+      });
+      option.classList.add('active');
+    });
+  });
 
 });
