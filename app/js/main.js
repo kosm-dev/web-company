aadb0483589f0998aab52412e37e66cc3612738d
kosm-dev <72253360+kosm-dev@users.noreply.github.com>
2023-03-27 13:39:05 +0300

    added adaptive page home

--- app/js/main.js
+++ app/js/main.js
@@ -1,33 +1,102 @@
-// Получаем все дропдауны
+"use strict";
+document.addEventListener("DOMContentLoaded", () => {
 
-const dropdowns = document.querySelectorAll('.dropdown');
+  // dropdowns
+  const dropdowns = document.querySelectorAll('.dropdown');
+  dropdowns.forEach(dropdown => {
+    const select = dropdown.querySelector('.select'),
+      caret = dropdown.querySelector('.caret'),
+      menu = dropdown.querySelector('.menu'),
+      options = dropdown.querySelectorAll('.menu li'),
+      selected = dropdown.querySelector('.selected');
 
-dropdowns.forEach(dropdown => {
-  const select = dropdown.querySelector('.select'),
-    caret = dropdown.querySelector('.caret'),
-    menu = dropdown.querySelector('.menu'),
-    options = dropdown.querySelectorAll('.menu li'),
-    selected = dropdown.querySelector('.selected');
+    select.addEventListener('click', () => {
+      select.classList.toggle('select-clicked');
+      caret.classList.toggle('caret-rotate');
+      menu.classList.toggle('menu-open');
+    });
 
-  select.addEventListener('click', () => {
-    select.classList.toggle('select-clicked');
-    caret.classList.toggle('caret-rotate');
-    menu.classList.toggle('menu-open');
-  });
+    options.forEach(option => {
+      option.addEventListener('click', () => {
+        selected.innerText = option.innerText;
 
-  options.forEach(option => {
-    option.addEventListener('click', () => {
-      selected.innerText = option.innerText;
+        select.classList.remove('select-clicked');
+        caret.classList.remove('caret-rotate');
+        menu.classList.remove('menu-open');
 
-      select.classList.remove('select-clicked');
-      caret.classList.remove('caret-rotate');
-      menu.classList.remove('menu-open');
-
-      options.forEach(option => {
-        option.classList.remove('active');
+        options.forEach(option => {
+          option.classList.remove('active');
+        });
+        option.classList.add('active');
       });
-      option.classList.add('active');
     });
   });
+
+  // button ontop
+
+  const onTop = document.querySelector(".ontop__btn");
+
+  window.onscroll = () => {
+    if (window.scrollY > 700) {
+      onTop.classList.remove('ontop__btn-hide');
+    } else if (window.scrollY < 700) {
+      onTop.classList.add('ontop__btn-hide');
+    }
+  }
+
+  onTop.addEventListener("click", () => {
+    window.scrollTo(0, 0);
+  });
+
+  // burger-menu
+
+  const burger = document.querySelector('.burger'),
+    menu = document.querySelector('.header__menu'),
+    body = document.querySelector('body'),
+    langMobile = document.querySelector('.language-mobile'),
+    socialsMobile = document.querySelector('.socials-mobile');
+
+  burger.addEventListener('click', () => {
+    burger.classList.toggle('burger--active');
+    menu.classList.toggle('header__menu--active');
+    body.classList.toggle('lock');
+    langMobile.classList.toggle('language-mobile--active');
+    socialsMobile.classList.toggle('socials-mobile--active');
+  });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 });
